63695ca06c666e91b24c1c3867b3e614425f7a3b
johnburitto <79087305+johnburitto@users.noreply.github.com>
2023-02-13 16:27:48 +0200

    Dice Roller, visual version

--- app/src/main/java/com/johnburitto/diceroller/MainActivity.kt
+++ app/src/main/java/com/johnburitto/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.johnburitto.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,8 +20,21 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
 
         val dice = Dice(6)
-        val resultTextView : TextView = findViewById(R.id.textView)
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        resultTextView.text = dice.roll().toString();
+        diceImage.setImageResource(getDiceImage(dice))
+    }
+
+    private fun getDiceImage(dice: Dice): Int {
+        when (dice.roll()) {
+            1 -> return R.drawable.dice_1
+            2 -> return R.drawable.dice_2
+            3 -> return R.drawable.dice_3
+            4 -> return R.drawable.dice_4
+            5 -> return R.drawable.dice_5
+            6 -> return R.drawable.dice_6
+        }
+
+        return 0;
     }
 }
